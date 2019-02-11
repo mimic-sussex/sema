3f0d6d045a1e729f77884f5b109f1003d77d95d0
Francisco Bernardo <franxico@gmail.com>
2019-02-11 14:58:41 +0000

    added build/ to gitignore

--- src/dndTree.js
+++ src/dndTree.js
@@ -26,12 +26,12 @@ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
 
 
-// [FB]: Added require 
+// [FB]: Added require
 var d3 = require('d3')
 
 
 // Get JSON data
-treeJSON = d3.json("flare.json", function(error, treeData) {
+treeJSON = d3.json("../flare.json", function(error, treeData) {
 
     // Calculate total nodes, max label length
     var totalNodes = 0;
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,29 @@
-module.exports = {
-  // entry: './test/test.web.js',
-  mode: 'development',
-  entry: {
-      app: './src/main.js'
-  },
-  output: {
-    path: __dirname + '/build',
-    publicPath: '/build/',
-    filename: 'bundle.js'
-  }
+"use strict";
 
-}
+// Required packages
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+
+const outputDir = "./build/";
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, outputDir)
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }]
+  },
+  plugins: [
+    new CleanWebpackPlugin([outputDir]),
+    new MonacoWebpackPlugin({
+      languages: ['javascript', 'typescript', 'handlebars', 'html', 'css']
+    })
+  ]
+};
