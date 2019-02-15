2ac41caf3799bed7e8aa2dccf46f6213f991b574
Francisco Bernardo <franxico@gmail.com>
2019-02-15 01:29:34 +0000

    maxiEngine is singleton and Monosynth is a prototype

--- src/maxiLibEngine.js
+++ src/maxiLibEngine.js
@@ -1,22 +1,34 @@
+import MaxiLib from './maxiLib';
 
-// var MaxiLibEngine = function () {
+// export default const MaxiLibEngine = function () {
+//     "use strict";
+//     if (MaxiLibEngine._instance) {
+//         //this allows the constructor to be called multiple times
+//         //and refer to the same instance. Another option is to
+//         //throw an error.
+//         return MaxiLibEngine._instance;
+//     }
+//     MaxiLibEngine._instance = this;
+//     //Foo initialization code
 //
-//   if (arguments.callee._singletonInstance) {
-//     return arguments.callee._singletonInstance;
-//   }
+//     var maxiLib = window.MaxiLib();
+//     var audio = new maxiLib.maxiAudio();
+//     var timer = new maximJs.maxiOsc(); //this is the metronome
+//     var currentCount, lastCount; //these values are used to check if we have a new beat this sample
+//     var mix;
+//     var monosynthLoaded;
 //
-//   arguments.callee._singletonInstance = this;
+//     audio.init();
 //
-//   this.Interpret = function (line) {
-//     console.log(line);
-//   };
-// }
 //
-// if (typeof module === "object" && module.exports) {
-//   module.exports = MaxiLibEngine;
+//     loadSamples();
 // };
 
-import MaxiLib from './maxiLib';
+// MaxiLibEngine.getInstance = function () {
+//     "use strict";
+//     return MaxiLibEngine._instance || new MaxiLibEngine();
+// }
+
 
 export default class MaxiLibEngine {
 
@@ -28,20 +40,16 @@ export default class MaxiLibEngine {
     var timer = new maximJs.maxiOsc(); //this is the metronome
     var currentCount, lastCount; //these values are used to check if we have a new beat this sample
 
-    var
-    var monosynthLoaded;
-
     var mix;
+
+    var monosynthLoaded;
 
     audio.init();
 
     loadSamples();
-
   }
 
-
-
-  function loadSamples(){
+  function loadSamples() {
     audio.loadSample("909b.wav", kick);
     audio.loadSample("909.wav", snare);
     audio.loadSample("909closed.wav", closedHat);
@@ -56,65 +64,62 @@ export default class MaxiLibEngine {
 
   function play() {
 
-    audio.play = function(){
-    	//so this first bit is just a basic metronome so we can hear what we're doing.
-    	currentCount=Math.round(timer.phasor(8));//this sets up a metronome that ticks every 2 seconds
+    audio.play = function() {
+      //so this first bit is just a basic metronome so we can hear what we're doing.
+      now = Math.round(timer.phasor(8)); //this sets up a metronome that ticks every 2 seconds
 
-    	if (lastCount!=currentCount) {//if we have a new timer int this sample, play the sound
+      if (oldClock != now) { //if we have a new timer int this sample, play the sound
 
-    		ADSR.trigger = 1;//trigger the envelope from the start
+        Monosynth.trigger(); //trigger the envelope from the start
 
-    		// console.log("tick\n");//the clock ticks
+        // console.log("tick\n");//the clock ticks
 
-    		lastCount=0;//set lastCount to 0
-    	}
+        oldClock = 0; //set lastCount to 0
+      }
 
-    	 //and this is where we build the synth
+      mix = 0.0;
 
-        if(monosynthLoaded)
-          playMonosynth();
+      //and this is where we build the synth
 
-        this.output = mix;
+      if (monosynthLoaded)
+        mix+=Monosynth.play();
+
+      this.output = mix;
     }
   }
 
-  function loadMonosynth(a=1000, d=1 , s=1, r=1000){
+  function Monosynth(a = 1000, d = 1, s = 1, r = 1000) {
 
-    var VCO1 = new maximJs.maxiOsc();
-    var VCO2 = new maximJs.maxiOsc();
-    var LFO1 = new maximJs.maxiOsc();
-    var LFO2 = new maximJs.maxiOsc();
+    this.VCO1 = new maximJs.maxiOsc();
+    this.VCO2 = new maximJs.maxiOsc();
+    this.LFO1 = new maximJs.maxiOsc();
+    this.LFO2 = new maximJs.maxiOsc();
+    this.VCF = new maximJs.maxiFilter();
+    this.ADSR = new maximJs.maxiEnv();
 
-    var VCF = new maximJs.maxiFilter();
-    var ADSR = new maximJs.maxiEnv();
+    this.VCO1out = null;
+    this.VCO2out = null;
+    this.LFO1out = null;
+    this.LFO2out = null;
+    this.VCFout = null;
+    this.ADSRout = null;
 
-    var VCO1out, VCO2out, LFO1out, LFO2out, VCFout, ADSRout;
-
-    ADSR.setAttack(a);
-    ADSR.setDecay(d);
-    ADSR.setSustain(s);
-    ADSR.setRelease(r);
-
-    monosynthLoaded = true;
+    this.ADSR.setAttack(a);
+    this.ADSR.setDecay(d);
+    this.ADSR.setSustain(s);
+    this.ADSR.setRelease(r);
   }
 
-  function playMonosynth(){
-
-    ADSR.trigger=1;
-    ADSRout=ADSR.adsr(1.0,ADSR.trigger);
-
-    LFO1out=LFO1.sinebuf(0.2);//this lfo is a sinewave at 0.2 hz
-
-    VCO1out=VCO1.pulse(55,0.6);//here's VCO1. it's a pulse wave at 55 hz, with a pulse width of 0.6
-    VCO2out=VCO2.pulse(110+LFO1out,0.2);//here's VCO2. it's a pulse wave at 110hz with LFO modulation on the frequency, and width of 0.2
-
-    VCFout=VCF.lores((VCO1out+VCO2out)*0.5, ADSRout*10000, 10);//now we stick the VCO's into the VCF, using the ADSR as the filter cutoff
-
-    mix=VCFout*ADSRout;//finally we add the ADSR as an amplitude modulator
-
-    // ADSR.trigger=0;
+  Monosynth.prototype.trigger = function (){
+    this.ADSR.trigger = 1;
   }
 
-
-
+  Monosynth.prototype.play = function () {
+    this.ADSRout = this.ADSR.adsr(1.0, this.ADSR.trigger);
+    this.LFO1out = this.LFO1.sinebuf(0.2); //this lfo is a sinewave at 0.2 hz
+    this.VCO1out = this.VCO1.pulse(55, 0.6); //here's VCO1. it's a pulse wave at 55 hz, with a pulse width of 0.6
+    this.VCO2out = this.VCO2.pulse(110 + this.LFO1out, 0.2); //here's VCO2. it's a pulse wave at 110hz with LFO modulation on the frequency, and width of 0.2
+    this.VCFout = this.VCF.lores((this.VCO1out + this.VCO2out) * 0.5, this.ADSRout * 10000, 10); //now we stick the VCO's into the VCF, using the ADSR as the filter cutoff
+    return this.VCFout * this.ADSRout; //finally we add the ADSR as an amplitude modulator
+  }
 }
