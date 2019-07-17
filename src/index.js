552e0f1451595c9bbf2c79991e48de5598344106
Chris Kiefer <chrisk13fer@gmail.com>
2019-07-17 13:12:44 +0100

    temp

--- src/index.js
+++ src/index.js
@@ -36,7 +36,6 @@ import 'codemirror/keymap/vim.js';
 import 'codemirror/lib/codemirror.css';
 
 import langSketch from './language/langSketch';
-import { hidden } from 'ansi-colors';
 
 let audio;
 
@@ -50,6 +49,7 @@ let evalTS = 0;
 
 let tfW = new tfWorker();
 tfW.onmessage = (e) => {
+  // console.log(e);
   window.AudioEngine.postMessage(e.data);
 }
 
@@ -111,8 +111,6 @@ function createEditor1() {
 }
 
 
-
-
 function createEditor2() {
   let defaultEditorCode2 = "//js";
   let editor2code = window.localStorage.getItem("editor2");
@@ -134,29 +132,6 @@ function createEditor2() {
   });
   editor2.setSize('100%', '100%');
 }
-
-function createEditor3() {
-  let defaultEditorCode3 = "//BNF grammar";
-  let editor3code = window.localStorage.getItem("editor3");
-  if (editor3code)
-    defaultEditorCode3 = editor3code;
-
-  editor3 = CodeMirror(document.getElementById('editor3'), {
-    value: defaultEditorCode3,
-    lineNumbers: true,
-    mode: "javascript",
-    theme: "idea",
-    lineWrapping: true,
-    extraKeys: {
-      // ["Cmd-Enter"]: () => evalEditor3Expression(),
-      // ["Ctrl-Enter"]: () => evalEditor3Expression(),
-      ["Shift-Enter"]: () => evalEditor3ExpressionBlock(),
-    }
-
-  });
-  editor2.setSize('100%', '100%');
-}
-
 
 function createControls() {
 
@@ -181,24 +156,6 @@ function createControls() {
   testButton.textContent = `Test`;
   container.appendChild(testButton);
   testButton.addEventListener("click", () => runTest());
-
-  const startAudioButton = document.getElementById('buttonStartAudio');
-  startAudioButton.onclick = () => {
-    let overlay = document.getElementById('overlay');
-    overlay.style.visibility = 'hidden';
-    playAudio();
-  }
-
-  const containerTabs = document.getElementById("containerTabs");
-  const modelButton = document.createElement("button");
-  modelButton.textContent = `Model`;
-  containerTabs.appendChild(modelButton);
-  modelButton.addEventListener("click", () => playAudio(editor1));
-  const grammarButton = document.createElement("button"); 
-  grammarButton.textContent = `Grammar`;
-  containerTabs.appendChild(grammarButton);
-  grammarButton.addEventListener("click", () => playAudio(editor1));
-
 
 }
 
@@ -299,29 +256,27 @@ function stopAudio() {
     window.AudioEngine.stop();
 }
 
-function increaseVolume() {
-  if (window.AudioEngine !== undefined)
-    window.AudioEngine.more('gainSyn');
-}
-
-function decreaseVolume() {
-  if (window.AudioEngine !== undefined)
-    window.AudioEngine.less('gainSyn');
-}
-
-function changeSynth() {
-  if (window.AudioEngine !== undefined)
-    window.AudioEngine.changeSynth();
-}
+// function increaseVolume() {
+//   if (window.AudioEngine !== undefined)
+//     window.AudioEngine.more('gainSyn');
+// }
+//
+// function decreaseVolume() {
+//   if (window.AudioEngine !== undefined)
+//     window.AudioEngine.less('gainSyn');
+// }
+//
+// function changeSynth() {
+//   if (window.AudioEngine !== undefined)
+//     window.AudioEngine.changeSynth();
+// }
 
 function createAnalysers() {
 
 }
 
+
 document.addEventListener("DOMContentLoaded", () => {
-
-
-
 
   // document.getElementById('audioWorkletIndicator').innerHTML = AudioWorkletIndicator.AudioWorkletIndicator();
 
@@ -356,7 +311,7 @@ document.addEventListener("DOMContentLoaded", () => {
   createControls();
 
   oscIO.OSCResponder((msg) => {
-    console.log("OSC in:", msg);
+    // console.log("OSC in:", msg);
     window.AudioEngine.oscMessage(msg);
   });
 
--- src/machineLearning/tfjs.worker.js
+++ src/machineLearning/tfjs.worker.js
@@ -4,12 +4,22 @@ importScripts("https://cdn.jsdelivr.net/npm/@tensorflow/tfjs");
 // let a = tf.tensor([100]);
 var geval = eval; // puts eval into global scope https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval
 geval("var next = (x) => {return 0;}");
+// 
+// var sema = {
+//   save: (name, val) => {
+//     postMessage({
+//       "function": "save",
+//       "name:": name,
+//       "val": val
+//     });
+//   }
+// };
 
 onmessage = (m) => {
   if ('eval' in m.data) {
     let evalRes = geval(m.data.eval);
     // let evalRes = Function(`return (${m.data.eval})`)();
-      console.log(evalRes);
+    console.log(evalRes);
   } else {
     // let freq = a.dataSync()[0];
     postMessage({
