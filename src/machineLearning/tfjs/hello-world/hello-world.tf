ee50e6128578d19727f1e3dbe5bcfbabb71f581b
Francisco Bernardo <f.bernardo@sussex.ac.uk>
2019-07-21 21:20:56 +0100

    model selection from hardcoded templated concluded

--- src/machineLearning/tfjs/hello-world/hello-world.tf
+++ src/machineLearning/tfjs/hello-world/hello-world.tf
@@ -1,4 +1,6 @@
 //js
+
+//create the model 
 var model = tf.sequential();
 model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
 model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
@@ -16,7 +18,7 @@ var test = (x) => { return model.predict(tf.tensor2d([x], [1, 1])).dataSync()[0]
 __________
 //route the test data into the model
 var w = 0;
-input = (id,x) => {console.log(">toModel: "+[id,x]); w=x};
+input = (id,x) => {console.log(">toModel:   "+[id,x]); w=x};
 __________
 //route the model predictions back to the live coding environment 
 output = (x) => {p = test(w);console.log(">fromModel: "+p); return p;}
