351efc9f2a1f5c82a46955050e045129b0ca47d2
Chris Kiefer <chrisk13fer@gmail.com>
2019-11-27 23:12:32 +0100

    update rate

--- src/audioEngine/maxi-processor.js
+++ src/audioEngine/maxi-processor.js
@@ -252,7 +252,7 @@ class MaxiProcessor extends AudioWorkletProcessor {
         //net clocks
         this.netClock.play(0.5, 15);
         let clockPhasor = this.netClock.getPhase(0) / (2 * Math.PI);
-        if (this.clockPhaseSharingInterval++ == 4000) {
+        if (this.clockPhaseSharingInterval++ == 400) {
           this.clockPhaseSharingInterval=0;
           let phase = this.netClock.getPhase(0);
           // console.log(`phase: ${phase}`);
