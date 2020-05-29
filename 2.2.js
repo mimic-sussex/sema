59f16bd87f927446702557ab95aaad1f9b9262e6
Francisco Bernardo <f.bernardo@sussex.ac.uk>
2020-05-29 18:16:09 +0100

    update for the last version

--- 2.2.js
+++
@@ -1,1 +0,0 @@
-(("undefined"!=typeof self?self:this).webpackJsonp=("undefined"!=typeof self?self:this).webpackJsonp||[]).push([[2],{212:function(e,s,f){"use strict";f.r(s);f(215),f(219),f(222),f(216),f(217),f(223),f(224),f(225),f(226),f(228),f(229),f(230),f(231),f(232),f(233),f(218),f(234),f(6),f(235),f(214),f(236),f(213),f(237),f(238),f(239)}}]);
