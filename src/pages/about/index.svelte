fe79478970edad35013e8ed01f178d1d9fb8795c
Francisco Bernardo <f.bernardo@sussex.ac.uk>
2021-03-18 14:38:22 +0000

    upgraded svelte-grid to v4.0.1, added About and blurb

--- src/pages/about/index.svelte
+++ src/pages/about/index.svelte
@@ -1,3 +1,51 @@
 <div class="center-all">
+  <div class="about">
     <h1>ABOUT</h1>
+    <h2>
+      What is <span class='sema'>sema</span>?
+    </h2>
+    <p>
+      <span>
+        Sema is a live coding environment for performing and composing music in realtime using simple live coding languages.
+      </span>
+      <span>
+        It is also a playground for designing and training machine learning networks and creating your own languages.
+      </span>
+    </p>
+    <p>
+      Sema is similar to other live coding environments and domain-specific languages for sound and music, such as SuperCollider, TidalCycles and Gibber.
+    </p>
+    <p>
+      Uniquely, Sema provides an integrated experience to machine learning and language design in your Web browser. It has a real-time, interactive interpreter for machine learning libraries (e.g. <a href="https://www.tensorflow.org/js/">tensorflow.js</a> and a compiler for <a href="https://hardmath123.github.io/earley.html">BNF grammars</a>.
+    </p>
+    <br>
+    <h2>
+      The <span class='sema'>sema</span> live coding community
+    </h2>
+    <p>
+      <span>
+        The playground
+      </span>
+    </p>
+    <p>
+      The discord community
+    </p>
+    <p>
+      <span>
+        The github repos
+      </span>
+    </p>
+  </div>
 </div>
+
+<style>
+  .about {
+    width: 100vw;
+    padding: 20px;
+    color: #999999;
+  }
+
+  .sema {
+    color: #FFFFFF;
+  }
+</style>
