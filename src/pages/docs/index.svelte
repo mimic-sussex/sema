4cd098f6aa460d565ee3c902b6bc23777b392b82
smilo7 <smilo@protonmail.ch>
2021-04-11 22:20:46 +0100

    docs now consume and display markdown from dist

---
+++ src/pages/docs/index.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { tick, onMount } from 'svelte';
+    import { url, params } from '@roxi/routify';
+    import marked from 'marked';
+    
+
+    function handleClick(){
+        console.log('AKSJDHKJASHDKJAHDS')
+    //console.log('HERE!!!');
+    }
+
+</script>
+
+
+<button on:click={handleClick}>
+    clickme
+</button>
