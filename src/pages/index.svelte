3f2317b5c899350cb2315396729cd5e4518eb404
Francisco Bernardo <f.bernardo@sussex.ac.uk>
2021-04-13 02:14:41 +0100

    added social media icons to landing page

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -112,6 +112,7 @@
           id="svg2"
           viewBox="0 0 500 395.89"
           version="1.1"
+          style='margin-top:auto;'
           inkscape:version="0.48.3.1 r9886"
           class='spiral-logo'
         >
@@ -492,6 +493,56 @@
     <div class="subtitle type-logo">
       <h2>a playground for livecoding Music and AI</h2>
     </div>
+    <div class="container-social-links">
+      <!-- svelte-ignore a11y-missing-attribute -->
+      <a href='https://github.com/mimic-sussex/sema' target='_blank'>
+        <svg class="octicon octicon-mark-github color-text-white"
+              viewBox="0 0 16 16"
+              version="1.1"
+              width="38"
+              aria-hidden="true"
+              style='margin: 1em 1em 1em 1em'
+              >
+              <path fill-rule="evenodd"
+                    d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+                    >
+              </path>
+        </svg>
+      </a>
+      <a href='https://www.youtube.com/channel/UCMJVYzJaZBNnKP6f0le7Lxg'
+          target='_blank'
+          >
+        <svg version="1.0"
+              id="Layer_1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              x="0px" y="0px"
+              viewBox="0 0 158 110"
+
+              width='44'
+              xml:space="preserve"
+              style='margin: 1em 1em 1em 1em'
+              >
+          <path fill="#FFFFFF"
+                d="M154.4,17.5c-1.8-6.7-7.1-12-13.9-13.8C128.2,0.5,79,0.5,79,0.5s-48.3-0.2-60.6,3c-6.8,1.8-13.3,7.3-15.1,14 C0,29.7,0.3,55,0.3,55S0,80.3,3.3,92.5c1.8,6.7,8.4,12.2,15.1,14c12.3,3.3,60.6,3,60.6,3s48.3,0.2,60.6-3c6.8-1.8,13.1-7.3,14.9-14c3.3-12.1,3.3-37.5,3.3-37.5S157.7,29.7,154.4,17.5z M63.9,79.2V30.8L103.2,55L63.9,79.2z"/>
+        </svg>
+      </a>
+      <a href='https://discord.gg/nNZMJfUHrS'
+          target='_blank'
+          >
+        <svg id="Layer_1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 245 240"
+              width='48'
+              style='margin: 1em 1em 1em 1em'
+              >
+              <style>.st0{fill:#FFFFFF;}</style>
+              <path class="st0"
+                  d="M104.4 103.9c-5.7 0-10.2 5-10.2 11.1s4.6 11.1 10.2 11.1c5.7 0 10.2-5 10.2-11.1.1-6.1-4.5-11.1-10.2-11.1zM140.9 103.9c-5.7 0-10.2 5-10.2 11.1s4.6 11.1 10.2 11.1c5.7 0 10.2-5 10.2-11.1s-4.5-11.1-10.2-11.1z"/><path class="st0" d="M189.5 20h-134C44.2 20 35 29.2 35 40.6v135.2c0 11.4 9.2 20.6 20.5 20.6h113.4l-5.3-18.5 12.8 11.9 12.1 11.2 21.5 19V40.6c0-11.4-9.2-20.6-20.5-20.6zm-38.6 130.6s-3.6-4.3-6.6-8.1c13.1-3.7 18.1-11.9 18.1-11.9-4.1 2.7-8 4.6-11.5 5.9-5 2.1-9.8 3.5-14.5 4.3-9.6 1.8-18.4 1.3-25.9-.1-5.7-1.1-10.6-2.7-14.7-4.3-2.3-.9-4.8-2-7.3-3.4-.3-.2-.6-.3-.9-.5-.2-.1-.3-.2-.4-.3-1.8-1-2.8-1.7-2.8-1.7s4.8 8 17.5 11.8c-3 3.8-6.7 8.3-6.7 8.3-22.1-.7-30.5-15.2-30.5-15.2 0-32.2 14.4-58.3 14.4-58.3 14.4-10.8 28.1-10.5 28.1-10.5l1 1.2c-18 5.2-26.3 13.1-26.3 13.1s2.2-1.2 5.9-2.9c10.7-4.7 19.2-6 22.7-6.3.6-.1 1.1-.2 1.7-.2 6.1-.8 13-1 20.2-.2 9.5 1.1 19.7 3.9 30.1 9.6 0 0-7.9-7.5-24.9-12.7l1.4-1.6s13.7-.3 28.1 10.5c0 0 14.4 26.1 14.4 58.3 0 0-8.5 14.5-30.6 15.2z"
+                  />
+          </svg>
+      </a>
+    </div>
   </div>
   <canvas id="canvas"
           class='canvas-logo'
@@ -509,35 +560,27 @@
     position: absolute;
     /* top: 50px; */
     display:grid;
-    grid-template-rows: 65vh 20vh auto;
+    grid-template-rows: 65vh 15vh 5vh 1fr;
     overflow-y: hidden;
   }
 
   .container-svg{
     width: 40vw;
     height: 40vh;
-    padding: 4.5em 5em 4.5em 8em;
+    padding: 0em 5em 6em 8em;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: auto;
+  }
+
+  .container-social-links {
+    display: flex;
+    justify-items: center;
+    justify-content:space-around;
     margin-left: auto;
     margin-right: auto;
   }
 
-
-  h1 {
-    font-size: 9vw;
-    margin: 0px;
-    /* font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; */
-    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-    /* position: absolute; */
-    text-align:center;
-  }
-
-  h2 {
-    font-size: 2.5vw;
-    margin: 0px;
-    /* position: absolute; */
-    text-align:center;
-    /* top: 2em; */
-  }
 
   path {
     fill: white;
@@ -547,12 +590,34 @@
     color: white;
   }
 
+  h1 {
+    font-size: 7vw;
+    /* font-size: 6.5rem; */
+
+    margin: 0px;
+    /* font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; */
+    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    /* position: absolute; */
+    text-align:center;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+    margin: 0px;
+    /* position: absolute; */
+    text-align:center;
+    /* top: 2em; */
+  }
+
+
   .title {
     grid-row: 2/2;
+    min-height: 100px;
   }
 
   .subtitle {
     grid-row: 3/3;
+    min-height: 100px;
   }
 
   .canvas-logo {
