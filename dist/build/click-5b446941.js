5237d2c9432d6ae735853b56137a7c542a836e01
Francisco Bernardo <f.bernardo@sussex.ac.uk>
2021-07-15 15:42:18 +0100

    remove dist folder

--- dist/build/click-5b446941.js
+++
@@ -1,6 +0,0 @@
-
-(function(l, r) { if (!l || l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (self.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; r.crossOrigin='anonymous'; l.getElementsByTagName('head')[0].appendChild(r) })(self.document);
-var click = "click.wav";
-
-export default click;
-//# sourceMappingURL=click-5b446941.js.map
--- dist/build/patterndrone2-10be29a1.js
+++
@@ -1,6 +0,0 @@
-
-(function(l, r) { if (!l || l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (self.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; r.crossOrigin='anonymous'; l.getElementsByTagName('head')[0].appendChild(r) })(self.document);
-var patterndrone2 = "patterndrone2.wav";
-
-export default patterndrone2;
-//# sourceMappingURL=patterndrone2-10be29a1.js.map
