1e663515ae7e451681d9d1796c37749a8b980cd2
smilo7 <smilo@protonmail.ch>
2021-07-26 16:22:04 +0100

    added clear to console and filtering for source of message

--- src/components/widgets/Console.svelte
+++ src/components/widgets/Console.svelte
@@ -4,7 +4,7 @@
   //import Logger from "../../utils/logger";
 
   import { Logger } from 'sema-engine';
-  import { rawConsoleLogs } from '../../stores/common.js'
+  import { rawConsoleLogs, consoleLogs } from '../../stores/common.js'
 
 
   let logger = new Logger();
@@ -20,6 +20,19 @@
   export let component;
   export let className;
   export { className as class };
+
+
+  //filter levels, by default log everything
+  let filter = {
+    processor: true,
+    main: true,
+    learner: true,
+    warn: false,
+    log: false,
+    info: false,
+    error: false,
+  }
+
 
   // export let append = '';
 
@@ -59,11 +72,15 @@
 	function eventListener(log){
 		console.log(log);
     $rawConsoleLogs = logger.rawLog;
+    $consoleLogs = logger.log;
+    console.log($consoleLogs);
 	}
 
   function clearLogs(){
+    console.log("clear logs getting called");
     logger.clear();
     $rawConsoleLogs = "";
+    $consoleLogs = [];
   }
 
   onMount(async () => {
@@ -72,17 +89,15 @@
       logger = new Logger();
     }
 
-		logger.addEventListener("onLog", eventListener)
-
-    //clear the log on Console mount!
-    clearLogs();
-
+		logger.addEventListener("onLog", eventListener);
+    console.log("TYPES", logger.types);
     //append = append + logger.log
     something( id, name, type, className, lineNumbers, hasFocus, theme, background, component );
   });
 
   onDestroy( async () => {
-
+    //clear the log on Console mount!
+    clearLogs();
 	})
 
 </script>
@@ -95,6 +110,7 @@
     width: 100%;
     height: 100%;
     border: none;
+    overflow-y: scroll;
   }
 
 	.scrollable-textarea {
@@ -108,31 +124,122 @@
     width: 100%;
     height: 100%;
     resize: none;
-    color: red;
+    color: white;
     overflow-y: scroll;
-    overflow-x: scroll;
   }
 
-  .console-logs {
-
+  .console-PROCESSOR {
+    color: green;
   }
 
-  .console-warns {
+  .console-LEARNER {
+    color: red;
+  }
 
+  .console-MAIN{
+    color: white;
+  }
+
+  .button {
+    background: none;
+    margin-top: 5px;
+    padding: 0px 0px;
+    border: 2px solid white;
+    padding: 4px 6px;
+  }
+
+  .button:hover {
+    background-color: grey;
+  }
+
+  .button:active {
+    background-color: white;
+  }
+
+  .console-settings{
+    overflow: hidden;
+    background-color: #333;
+    position: fixed; /* Set the navbar to fixed position */
+    width: 80%; /* Full width */
+    height: 10%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 10%;
+  }
+
+  label {
+    display: inline-block;
+    font-family: monospace
+  }
+
+  .section-header {
+    display: inline-block;
+    font-family: monospace;
+    text-decoration: underline;
   }
 
 </style>
 
-<div class='console-container scrollable-textarea'>
+<div class='console-container scrollable-textarea' bind:this={textArea}>
 
+  <div class="console-settings">
+
+    <button type="button" class="button" on:click={clearLogs}>🚫</button>
+    
+    <p class="section-header">Filter Source: </p>
+
+    <input type="checkbox" id="PROCESSOR" name="PROCESSOR" bind:checked={filter.processor}>
+    <label for="PROCESSOR">Processor</label>
+
+    <input type="checkbox" id="MAIN" name="MAIN" bind:checked={filter.main}>
+    <label for="MAIN">Main</label>
+
+    <input type="checkbox" id="LEARNER" name="LEARNER" bind:checked={filter.learner}>
+    <label for="LEARNER">Learner</label>
+
+    <!--
+    <p class="section-header">Filter Level: </p>
+
+    <input type="checkbox" id="level-log" name="level-log" bind:checked={filter.log}>
+    <label for="level-log">logs</label>
+
+    <input type="checkbox" id="level-error" name="level-error" bind:checked={filter.error}>
+    <label for="level-error">errors</label>
+    -->
+
+  </div>
+  <!--
+  <input type="checkbox" id={logger.type.processor} name={logger.type.processor}
+         checked>
+  <label for={logger.type.processor}>Processor</label>
+  -->
+
+  {#each $consoleLogs as {func, payload, source, type}, i}
+    {#if source == logger.types.processor && filter.processor != false}
+      <pre readonly class='console-PROCESSOR'>{source}{payload}</pre>
+    {:else if source == logger.types.learner && filter.learner != false}
+      <pre readonly class='console-LEARNER'>{source}{payload}</pre>
+    {:else if source == logger.types.main && filter.main != false}
+      <pre readonly class='console-MAIN'>{source}{payload}</pre>
+    {/if}
+
+    <!-- <pre readonly bind:this={ textArea } class='console-textarea'>{ type }</pre> -->
+  {/each}
+  <!-- <pre readonly bind:this={ textArea } class='console-textarea'>{ $rawConsoleLogs }</pre> -->
+
+</div>
+
+
+<!--
+<div class='console-container scrollable-textarea'>
+  
   <pre readonly
       bind:this={ textArea }
 
+      
       class='console-textarea'
       >{ $rawConsoleLogs }</pre>
-  <!-- <textarea readonly
-      bind:this={ textArea }
-      bind:value
-      class='console-textarea'
-      ></textarea> -->
+
 </div>
+-->
