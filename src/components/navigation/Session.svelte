a6dc8f49b74e4842a6bdc3ffcac7a86a707e29e3
smilo7 <smilo7@users.noreply.github.com>
2021-09-28 13:12:55 +0100

    Added bootsrap profile icon next to username to help indicate what it is to the user

--- src/components/navigation/Session.svelte
+++ src/components/navigation/Session.svelte
@@ -80,6 +80,9 @@
     text-decoration: none;
   }
 
+	.profile-icon{
+		vertical-align: baseline;
+	}
 
 </style>
 
@@ -91,6 +94,10 @@
 			<a href="/admin"
 				style='color: {$siteMode === 'dark'? 'white': 'black'};'
 				>
+				<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="profile-icon" viewBox="0 0 16 16">
+					<path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+					<path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+				</svg>
 			{ $userName }</a>
 			{#if $avatarSrc}
 				<div class='container-session-avatar'>
