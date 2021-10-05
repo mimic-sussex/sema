0d66878afb74ba3b6ba0c2714c9afbccc69368fd
smilo7 <smilo7@users.noreply.github.com>
2021-10-05 18:07:39 +0100

    projects page now looks much better. settings dropdown here has icons too

--- src/components/admin/Records.svelte
+++ src/components/admin/Records.svelte
@@ -51,8 +51,10 @@
 	}
 
 	$: $records = getAllProjects();//fetchRecords(); //promise is reactive to changes in url docId and links since they load asynchrynously
-	$: updateProjectPage(projectPage);
+	$: updateProjectPage(projectPage); //reactive statement reacts to changes in projectPage variable
 
+
+	//updates which list of projects is on display (my projects or all projects)
 	const updateProjectPage = async (projectPage) => {
 		if (projectPage == 'my-projects'){
 			console.log('refreshing my-projects page', projectPage)
@@ -222,6 +224,7 @@
 .container-records {
 	overflow: auto;
 	width: 100%;
+	/* background-color: #333; */
 }
 
 .record-name {
@@ -238,12 +241,18 @@
 	margin-bottom: 0.5em;
 }
 
+
 table {
 	width:100%;
+	border-collapse: collapse;
 }
 
 th {
 	text-align:left
+}
+
+.record-entry:hover {
+	background-color: #333;
 }
 
 input[type=radio] {
@@ -252,18 +261,17 @@ input[type=radio] {
   margin: 2px 0 0 2px;
 }
 
-/* .container-project-filter {
-	display: inline-block;
-	width:100%;
-} */
-
 label {
 	float: left;
 	clear: none;
 	display: block;
 	padding: 0px 1em 0px 8px;
 }
-    
+
+.file-name {
+ color: white;
+}
+
 .dropdown {
   float: left;
   overflow: hidden;
@@ -281,7 +289,7 @@ label {
 .dropdown-content {
   display: none;
   position: absolute;
-  background-color: #f9f9f9;
+  background-color: #282828;
   min-width: 160px;
   box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
   z-index: 1;
@@ -289,7 +297,7 @@ label {
 
 .dropdown-content a {
   float: none;
-  color: black;
+  color: #f9f9f9;
   padding: 12px 16px;
   text-decoration: none;
   display: block;
@@ -297,7 +305,7 @@ label {
 }
 
 .dropdown-content a:hover {
-  background-color: #ddd;
+  background-color: #404040;
 }
 
 .dropdown:hover .dropdown-content {
@@ -313,22 +321,72 @@ label {
 	fill: #282828;
 }
 
+.container-project-filter {
+	/* display: inline-block; */
+	/* width:100%; */
+	border-bottom: 1px solid white;
+}
+
+.project-tab {
+
+}
+
+button {
+	background: none;
+	border: none;
+	/* border-bottom: 1px solid white; */
+	border-radius: 0;
+	margin: 0;
+	color: #ccc;
+}
+
+.project-tab-selected {
+	border-bottom: 3px solid white;
+	/* background-color: red; */
+}
+
+.svg-icon-div {
+	
+	/* white-space:nowrap;
+	overflow: hidden; */
+	/* display: flex; */
+	float: right;
+	display: inline-flex;
+  align-self: center;
+	/* text-align: right; */
+}
+
+.fork-icon {
+	fill: grey;
+}
+
+.share-icon {
+	fill: grey;
+}
+
+.delete-icon {
+	fill: grey;
+}
+
 </style>
 <!-- 
 <button on:click={getMyProjects} >My Projects</button>
 <button on:click={getAllProjects}>Browse Projects</button> -->
 
-<p>{projectPage} selected</p>
+<!-- <p>{projectPage} selected</p> -->
 
 <div class="container-project-filter">
-	<input type="radio" id="my-projects-radio" name="project-filter" value="my-projects" bind:group={projectPage}>
+	<!-- <input type="radio" id="my-projects-radio" name="project-filter" value="my-projects" bind:group={projectPage}>
 	<label for="my-projects-radio">My Projects</label>
 	<input type="radio" id="all-projects-radio" name="project-filter" value="all-projects" bind:group={projectPage}>
-	<label for="my-projects-radio">Browse All Projects</label>
+	<label for="my-projects-radio">Browse All Projects</label> -->
+
+	<button class:project-tab-selected={projectPage == "my-projects"} on:click={() => projectPage = "my-projects"}>My Projects</button>
+	<button class:project-tab-selected={projectPage == "all-projects"} on:click={() => projectPage = "all-projects"}>All Projects</button>
+
 </div>
 
-<div class='container-records' in:slide>
-	
+<div class='container-records' in:slide>	
 		
 	{#await $records }
 		<p>...waiting</p>
@@ -344,65 +402,84 @@ label {
 				</tr>
 
 				{#each $records as record }
-					<tr>
-
+					<tr class="record-entry">
 						<td>
-						<a href="playground/{ record.id }"
+						<a class="file-name" href="playground/{ record.id }"
 								on:click={ setPlaygroundFromRecord(record) }
 								>
 								<span class='record-name'
 								>{ record.name }
 								</span>
 						</td>
-					
-					
-					<td>
-						<!-- {( record.isPublic ? "Public": 'Private' )} -->
-						{#if record.isPublic}
-							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="visibility-icon" viewBox="0 0 16 16" on:click={toggleVisibility(record.id, false)}>
-								<path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z"/>
-							</svg>
-						{:else}
-							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="visibility-icon" viewBox="0 0 16 16" on:click={toggleVisibility(record.id, true)}>
-								<path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z"/>
-							</svg>
-						{/if}
-					</td>
 
-					<td>
-						{#if record.author}
-							{#if record.author.username}
-								{record.author.username}
-							{:else}
-								No Username
-							{/if}
-						{/if}
-					</td>
-
-					<td>
-						{ getDateStringFormat(record.updated) }
-					</td>
-
-					<td>
-						<!-- <button>Fork</button> -->
-						
-						<div class="dropdown">
-							<button class="dropbtn">
-
-								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="settings-icon" viewBox="0 0 16 16">
-									<path fill-rule="evenodd" d="M11.5 2a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM9.05 3a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0V3h9.05zM4.5 7a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM2.05 8a2.5 2.5 0 0 1 4.9 0H16v1H6.95a2.5 2.5 0 0 1-4.9 0H0V8h2.05zm9.45 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm-2.45 1a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0v-1h9.05z"/>
+						<td>
+							<!-- {( record.isPublic ? "Public": 'Private' )} -->
+							{#if record.isPublic}
+								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="visibility-icon" viewBox="0 0 16 16" on:click={toggleVisibility(record.id, false)}>
+									<path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z"/>
 								</svg>
+							{:else}
+								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="visibility-icon" viewBox="0 0 16 16" on:click={toggleVisibility(record.id, true)}>
+									<path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z"/>
+								</svg>
+							{/if}
+						</td>
 
-							</button>
-							<div class="dropdown-content">
-								<a href={'#'} on:click={forkProject(record.id)}>Fork</a>
-								<a href={'#'} on:click={shareProject(record.id)}>Share</a>
-								<a href={'#'} on:click={deleteProject(record.id)}>Delete</a>
-							</div>
-						</div> 
+						<td>
+							{#if record.author}
+								{#if record.author.username}
+									{record.author.username}
+								{:else}
+									No Username
+								{/if}
+							{/if}
+						</td>
+
+						<td>
+							{ getDateStringFormat(record.updated) }
+						</td>
+
+						<td>
+							<!-- <button>Fork</button> -->
+							
+							<div class="dropdown">
+								<button class="dropbtn">
+
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="settings-icon" viewBox="0 0 16 16">
+										<path fill-rule="evenodd" d="M11.5 2a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM9.05 3a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0V3h9.05zM4.5 7a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM2.05 8a2.5 2.5 0 0 1 4.9 0H16v1H6.95a2.5 2.5 0 0 1-4.9 0H0V8h2.05zm9.45 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm-2.45 1a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0v-1h9.05z"/>
+									</svg>
+
+								</button>
+								<div class="dropdown-content">
+									<a href={'#'} on:click={forkProject(record.id)}>Fork
+										<div class="svg-icon-div">
+											<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="fork-icon">
+												<path fill-rule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"></path>
+											</svg>
+										</div>
+									</a>
+									
+									<a href={'#'} on:click={shareProject(record.id)}>Share
+										<div class="svg-icon-div">
+											<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="share-icon" viewBox="0 0 16 16">
+												<path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z"/>
+											</svg>
+										</div>
+									</a>
+									
+									<a href={'#'} on:click={deleteProject(record.id)}>Delete
+										<div class="svg-icon-div">
+											<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="delete-icon" viewBox="0 0 16 16">
+												<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+												<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+											</svg>
+										</div>
+									</a>
+								</div>
+							</div> 
 
 
-					</td>
+						</td>
 
 				</tr>
 
@@ -410,7 +487,7 @@ label {
 				{/each}
 			</table>
 
-			<button>New Project</button>
+			<!-- <button>New Project</button> -->
 
 		{:else}
 			<p style="color: red">No projects yet. Go to the playground and make one!</p>
