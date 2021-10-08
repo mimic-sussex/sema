2422999d109f10aa52e3d93a37043685779cac4f
smilo7 <smilo7@users.noreply.github.com>
2021-10-08 16:56:39 +0100

    some checks to see if routifys folder structure routes are working for [playgroundId]

--- src/pages/playground/[playgroundId]/_layout.svelte
+++ src/pages/playground/[playgroundId]/_layout.svelte
@@ -1,28 +1,42 @@
 <script>
 
+	import { tick, onMount, onDestroy} from 'svelte';
+
+	import { url, params, ready, isActive, route, afterPageLoad, beforeUrlChange, goto} from "@roxi/routify";
+
   import {
     isSaveOverlayVisible,
     loadEnvironmentSnapshotEntries,
 		items,
 		name,
 		uuid
-  } from  "../../../stores/playground.js"
+  } from  "../../../stores/playground";
 
-  import { url, params, ready, isActive, route, afterPageLoad, beforeUrlChange, goto} from "@roxi/routify";
-
-	console.log($uuid)
-	console.log($url)
-	console.log($params)
+	// console.log($uuid)
+	// console.log($url)
+	// console.log($params)
 	// console.log($route)
-	console.log($name)
+	// console.log($name)
 	// console.log($beforeUrlChange)
 	// console.log($afterPageLoad)
+
+	// $: promise = logUUID($params.playgroundId);
+	
+	onMount( async () => {
+		console.log("[playground mount]");
+    console.log("DEBUG:routes/playground/"+$params.playgroundId+"/_layout:onMount");
+  });
+
+
+	function logUUID(param){
+		console.log("logging uuid", param);
+	}
 
 </script>
 
 <div>
-	{ $params.playgroundId }
-  { $name }
+	<!-- { $params.playgroundId } -->
+  <!-- { $name } -->
 	<!-- { params }
 	{ url }
 	{ url }
