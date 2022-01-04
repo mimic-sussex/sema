a74b22476ac8dac855b4b11908b34cc34dc8642c
smilo7 <smilo7@users.noreply.github.com>
2022-01-04 17:08:18 +0000

    Adding public user profile, userlist and user not found pages.

---
+++ src/pages/user/views/UserList.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let userList;
+</script>
+
+<h1>Sema users</h1>
+<span>Click a username to view their profile.</span>
+<br>
+
+{#if userList}
+  <ul>
+  {#each userList as user}
+    <li><a href="{`user/${user.username}`}" >{user.username}</a></li>
+  {/each}
+  </ul>
+{/if}
