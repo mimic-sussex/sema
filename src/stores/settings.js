7b547b851d05dae12ff5026a9a638d9a947d142f
smilo7 <smilo@protonmail.ch>
2022-01-06 18:36:32 +0000

    making engine settings drawer open by default

--- src/stores/settings.js
+++ src/stores/settings.js
@@ -2,4 +2,4 @@ import { writable, readable, get } from "svelte/store";
 
 export let screenSettings = writable(false);
 export let environmentSettings = writable(true);
-export let engineSettings = writable(false);
+export let engineSettings = writable(true);
