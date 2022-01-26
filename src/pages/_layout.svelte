3b0f60712322b6e7bb60ea8fe393101995873ef5
smilo7 <smilo@protonmail.ch>
2022-01-26 18:11:02 +0000

    removing old code comments in docs implementation

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -266,47 +266,9 @@
     const res1 = await fetch(document.location.origin + `/docs/docs.json`);
     const json = await res1.json();
     if (res1.ok){
-      //let tmpLinks = json;
-      //let tmpChosenDocs = tmpLinks[0].path;
       $links = json
-      //let result =  await getSubs(tmpLinks);
-      //$links = result;
     }
   }
-
-  //get subheadings for a page based on the h1 headers in the .md file.
-  //redundant, now we just get these when loading the markdown.
-  /*
-  async function getSubs(list){
-    for (let i=0;i<list.length;i++){
-        let currentHeadings = [];
-        if (list[i].container == true){
-          getSubs(list[i].children);
-        } else {
-          //get headings for that child
-          if(list[i].file != undefined){ // There is a call with undefined value when navigating to Playground
-            const res = await fetch(document.location.origin + `/docs/${list[i].file}.md`)
-            const text = await res.text();
-            if (res.ok) {
-              //get tokens from the marked lexer
-              let tokens = marked.lexer(text);
-              //loop through them
-              for (let i=0; i<tokens.length; i++){
-                if (tokens[i].type == "heading" && tokens[i].depth == 1){
-                  let heading = tokens[i].text;
-                  currentHeadings.push({heading: heading , route: heading.replace(/\s+/g, '-').toLowerCase(), active:false})
-                }
-              }
-              list[i].subs = currentHeadings;
-            } else {
-              throw new Error(text);
-            }
-          }
-        }
-    }
-    return list
-  }
-  */
 
   onMount( async () => {
     console.log('DEBUG: onMount! Root layout')
