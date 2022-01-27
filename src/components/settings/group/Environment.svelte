4b5698189393a37038be80eb3e88a429544fdd59
smilo7 <smilo@protonmail.ch>
2022-01-27 18:27:50 +0000

    tidying up conditional display of project name and launcher box in environment settings.

--- src/components/settings/group/Environment.svelte
+++ src/components/settings/group/Environment.svelte
@@ -4,6 +4,7 @@
   } from '../../../stores/common.js';
 
   import { isActive, goto, url, params } from "@roxi/routify";
+  import ContentLoader from 'svelte-content-loader';
 
 	import {
     onMount,
@@ -642,8 +643,88 @@
   {/if}
 {/if}
 
-<!--NAME PROJECT TEXT BOX-->
-{#if $isPublic && $isActive(`/playground`)}
+<!--PROJECT NAME BOX-->
+
+<!-- only show in playround -->
+{#if $isActive(`/playground`)} 
+  {#if $name != null}
+  <!-- PROJECT VISIBILITY ICON -->
+    {#if $isPublic}
+      <svg xmlns="http://www.w3.org/2000/svg" 
+        width="16" 
+        height="16" 
+        fill="currentColor" 
+        class="playground-visibility-icon" 
+        style=""
+        viewBox="0 0 16 16" 
+        >
+          <title>Public. This project will appear in the 'All Projects' tab.</title>
+          <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z"/>
+      </svg>
+    {:else if !$isPublic}
+      <svg xmlns="http://www.w3.org/2000/svg" 
+        width="16" 
+        height="16" 
+        fill="currentColor" 
+        class="playground-visibility-icon" 
+        style=""
+        viewBox="0 0 16 16" 
+        >
+          <title>Private. This project will only appear in the 'My Projects' tab.</title>
+          <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z"/>
+      </svg>
+    {/if}
+    
+    <!-- PROJECT NAME TEXT BOX-->
+    
+    {#if $params.playgroundId} 
+      <input type="text"
+              bind:value={ $name }
+              on:change={ onNameChange }
+              placeholder='Choose a project!'
+              style="{( $isActive(`/playground`) )? `visibility:visible;`: `visibility:collapse`}; margin-left: 2px;"
+              disabled={!$user}
+              />
+
+      <!-- Project browser launcher dropdown button -->
+      <button 
+      id = 'project-browser-launcher-button'
+      class="{ $siteMode === 'dark'? 'dropdown-button-dark' :'button-light' }"
+      title="project browser"
+      style="{( $isActive('/playground') )? `visibility:visible;`: `visibility:collapse`};
+      {($isProjectBrowserOverlayVisible)? 'background-color: #181a1d;' :''}
+      margin-left: -50px;"
+      on:click={ () => toggleProjectBrowser()}>
+
+        <div id='project-browser-launcher-button' class='icon-container' style='margin-top:5px;'>
+          <svg xmlns="http://www.w3.org/2000/svg" 
+          width="16" 
+          height="16" 
+          fill="currentColor" 
+          class="bi bi-chevron-down" 
+          id = 'project-browser-launcher-button'
+          viewBox="0 0 16 16"
+          style='{ ($isProjectBrowserOverlayVisible)? 'transform: rotate(180deg); transition: 0.3s;' :'transform: rotate(0deg); transition: 0.1s;'}'
+          >
+            <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+          </svg>
+        </div>
+      </button>
+    {/if}
+
+  {:else}
+  <!-- show loading -->
+    <!-- Loading... -->
+    <div class=''>
+      <ContentLoader primaryColor='grey' secondaryColor='#ccc' speed={0.4} width="200" height="16">
+        <rect x="0" y="0" rx="3" ry="3" width="200" height="16" />
+      </ContentLoader>
+    </div>
+  {/if}
+
+{/if}
+
+<!-- {#if $isPublic && $isActive(`/playground`)}
   <svg xmlns="http://www.w3.org/2000/svg" 
     width="16" 
     height="16" 
@@ -678,7 +759,7 @@
           disabled={!$user}
           />
 
-  <!-- Project browser launcher -->
+  
   <button 
   id = 'project-browser-launcher-button'
   class="{ $siteMode === 'dark'? 'dropdown-button-dark' :'button-light' }"
@@ -702,4 +783,4 @@
       </svg>
     </div>
   </button>
-{/if}
+{/if} -->
